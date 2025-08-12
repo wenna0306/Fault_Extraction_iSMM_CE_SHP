c6a1540ca59496588ecccc9151ce9ed32fc54ade
wenna0306 <74126994+wenna0306@users.noreply.github.com>
2025-08-12 16:45:57 +0800

    Update fault_extraction.py

--- fault_extraction.py
+++ fault_extraction.py
@@ -132,5 +132,6 @@ supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 data_dic = df_final.to_dict(orient="records")
 
 # Upsert data into Supabase table
-supabase.table("fault_AFT").upsert(data_dic, on_conflict=["fault_number"]).execute()
+supabase.table("fault_SHP").upsert(data_dic, on_conflict=["Fault Number"]).execute()
+
 
